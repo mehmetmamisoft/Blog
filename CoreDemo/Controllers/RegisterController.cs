b8c3c1006861f4c48ebbdf485a6ac95adfc041e1
mehmet seydal <mehemet01@hotmail.com>
2022-05-01 22:12:58 +0300

    1 MayısAdd project files.

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -23,10 +25,27 @@ namespace CoreDemo.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "deneme test";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index","Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            //işlem geçerliyse
+            if (results.IsValid)
+            {
+
+                p.WriterStatus = true;
+                p.WriterAbout = "deneme test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+
+
         }
     }
 }
