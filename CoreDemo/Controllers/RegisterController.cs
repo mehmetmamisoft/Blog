fa2882b5cd04e0cee3842343cdceed3e4fd132ad
mehmet seydal <mehemet01@hotmail.com>
2022-05-01 05:00:02 +0300

    Add project files.

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            p.WriterStatus = true;
+            p.WriterAbout = "deneme test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            var commentvalues = new List<UserComment>
+            {
+            new UserComment
+            {
+              ID=1,
+              UserName="Mehmet"
+            },
+             new UserComment
+            {
+              ID=2,
+              UserName="Murat"
+            },
+              new UserComment
+            {
+              ID=3,
+              UserName="Osman"
+            }
+            };
+            return View(commentvalues);
+        }
+    }
+}
